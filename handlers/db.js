fef62cb6ecdaed4a35bed1159fb5d6f49eda5a53
chessmankir <skornkir2@gmail.com>
2026-04-10 22:51:16 +0300

    fix

--- handlers/db.js
+++ handlers/db.js
@@ -1,18 +1,18 @@
     // db.js
     const { Pool } = require('pg');
     require('dotenv').config();
-   const pool = new Pool({
-      connectionString: process.env.SUPABASE_DATABASE_URL2,
+   /*const pool = new Pool({
+      connectionString: process.env.SUPABASE_DATABASE_URL,
       ssl: { rejectUnauthorized: false },
-    });
+    });*/
 
-   /* const pool = new Pool({
+    const pool = new Pool({
         host: "orgasedooysog.beget.app",
         port: 5432,
         user: "cloud_user",
         password: "w*sVwHf6Yn%*",
         database: "checkmate",
-    });*/
+    });
 
 
     module.exports = pool;
